81e4dd9f768c4eed71cc35fdc3d4526a6d1f98e4
Damien Dotta <damien.dotta@live.fr>
2024-10-07 14:02:26 +0200

    feat: remplacement de MASA par MASAF

--- _extensions/masa-default/bg.lua
+++ _extensions/masa-default/bg.lua
@@ -1,6 +1,6 @@
 function Header(el)
     if not el.attributes['background-image'] then
-        el.attributes['background-image'] = 'img/com_interne/PageStandard_Masa.svg'
+        el.attributes['background-image'] = 'img/com_interne/PageStandard_Masaf.svg'
     end
 
     return el
